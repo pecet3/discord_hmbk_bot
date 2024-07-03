1155331dcfde89da33c45d2a26de03909edeb631
pecet3 <pecet3107@gmail.com>
2024-07-03 17:51:35 +0200

    removing expired sessions

--- discord/handlePaint.go
+++ discord/handlePaint.go
@@ -19,7 +19,7 @@ func handlePaint(s *discordgo.Session, m *discordgo.MessageCreate, ps *paint.Pai
 	session := paint.PaintSession{
 		Id:         sessionId,
 		ImgBytesCh: make(chan []byte),
-		ExpiresAt:  time.Now().Add(4 * time.Hour),
+		ExpiresAt:  time.Now().Add(2 * time.Hour),
 	}
 
 	ps.AddSession(sessionId, session)
@@ -32,22 +32,45 @@ func handlePaint(s *discordgo.Session, m *discordgo.MessageCreate, ps *paint.Pai
 		url + "?session_id=" + sessionId
 	s.ChannelMessageSend(prvChannel.ID, prvMsg)
 
-	for {
-		imgBytes := <-session.ImgBytesCh
-		imgName := m.Author.Username + "_" + sessionId + ".png"
-		file := &discordgo.File{
-			Name:        imgName,
-			ContentType: "image/png",
-			Reader:      bytes.NewReader(imgBytes),
+	expiryCh := make(chan bool)
+	go func(expiryCh chan bool) {
+		for {
+			s, isExists := ps.GetSession(sessionId)
+			if !isExists {
+				break
+			}
+			if s.ExpiresAt.Before(time.Now()) {
+				expiryCh <- true
+				break
+			}
+			time.Sleep(5 * time.Second)
 		}
+	}(expiryCh)
 
-		msg := discordgo.MessageSend{
-			File:    file,
-			Content: "Oto piękny obraz autorstwa" + m.Author.Mention(),
+	for {
+		select {
+		case imgBytes := <-session.ImgBytesCh:
+			imgName := m.Author.Username + "_" + sessionId + ".png"
+			file := &discordgo.File{
+				Name:        imgName,
+				ContentType: "image/png",
+				Reader:      bytes.NewReader(imgBytes),
+			}
+
+			msg := discordgo.MessageSend{
+				File:    file,
+				Content: "Oto piękny obraz autorstwa" + m.Author.Mention(),
+			}
+			log.Printf("> Painting %s has been sent", imgName)
+			s.ChannelMessageSendComplex(m.ChannelID, &msg)
+			ps.RemoveSession(sessionId)
+			return
+		case isExpired := <-expiryCh:
+			if isExpired {
+				ps.RemoveSession(sessionId)
+				log.Printf("Paint session with id: %s, created by: %s has been removed (expired)", sessionId, m.Author.Username)
+				return
+			}
 		}
-		log.Printf("> Painting %s has been sent", imgName)
-		s.ChannelMessageSendComplex(m.ChannelID, &msg)
-		ps.RemoveSession(sessionId)
-		break
 	}
 }
