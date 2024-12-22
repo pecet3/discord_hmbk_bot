b1db4083189938a1dc0627bb775d122c45397aa8
Jakub <pecet3107@gmail.com>
2024-12-22 01:13:03 +0100

    okkk

--- discord/main.go
+++ discord/main.go
@@ -42,7 +42,7 @@ func Run(discord *discordgo.Session, ps *paint.PaintSessions) {
 	// Random Sessions handle loop
 	go func() {
 		for {
-			time.Sleep(time.Minute * 1)
+			time.Sleep(time.Minute * 100000000)
 			i := 0
 			lenRandomS := len(sessions.RandomS)
 			if lenRandomS == 0 {
@@ -50,12 +50,11 @@ func Run(discord *discordgo.Session, ps *paint.PaintSessions) {
 			}
 			winnerIndex := rand.Intn(lenRandomS)
 			log.Println("Winner index is: ", winnerIndex)
-			for uuid, u := range sessions.RandomS {
+			for _, u := range sessions.RandomS {
 				if i == winnerIndex {
 					discord.ChannelMessageSend(FONTANNA_ID, u.User.Username+" to totalny kozak")
 					break
 				}
-				sessions.RemoveRandomSession(uuid)
 				i++
 			}
 		}
